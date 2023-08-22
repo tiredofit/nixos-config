1241bba6971dc798822058e9e7e00ad7f5747648
Dave Conroy <dave@tiredofit.ca>
2023-08-22 10:23:06 -0700

    impermanence - Add btrbk snapshot support for /persist

--- modules/nixos/impermanence.nix
+++ modules/nixos/impermanence.nix
@@ -150,6 +150,20 @@ in
       };
     };
 
+    services = mkIf cfg_impermanence.enable {
+      btrbk = {
+        instances."btrbak" = {
+          settings = {
+            volume."/persist" = {
+              snapshot_create = "always";
+              subvolume = ".";
+              snapshot_dir = ".snapshots";
+            };
+          };
+        };
+      };
+    };
+
     security = mkIf cfg_impermanence.enable {
       sudo.extraConfig = ''
         Defaults lecture = never
