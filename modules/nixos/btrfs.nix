0471e1ed943acb3f0673fc9f1c6b03aba77effb5
Dave Conroy <dave@tiredofit.ca>
2023-08-22 10:18:52 -0700

    btrfs/btrbk - fix snapshot retention

--- modules/nixos/btrfs.nix
+++ modules/nixos/btrfs.nix
@@ -39,10 +39,10 @@ in
           onCalendar = "*-*-* *:00:00";
           settings = {
             timestamp_format = "long";
-            snapshot_preserve_min = "2d";
             preserve_day_of_week = "sunday" ;
             preserve_hour_of_day = "0" ;
-            target_preserve = "48h 10d 4w 12m 10y" ;
+            snapshot_preserve = "48h 10d 4w 12m 10y" ;
+            snapshot_preserve_min = "2d";
             volume."/home" = {
               snapshot_create = "always";
               subvolume = ".";
