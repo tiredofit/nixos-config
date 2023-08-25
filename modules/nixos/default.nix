3d5883a5d63d238e9aaa445e3a8263be486f92a5
Dave Conroy <dave@tiredofit.ca>
2023-08-25 16:37:58 -0700

    module - create host.feature.virtualization.flatpak

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -5,7 +5,9 @@ host_feature_btrfs = import ./feature/btrfs.nix;
 host_feature_encryption = import ./feature/encryption.nix;
 host_feature_impermanence = import ./feature/impermanence.nix;
 host_feature_powermanagement = import ./feature/power_management.nix;
+
 host_feature_virtualization-docker = import ./feature/virtualization-docker.nix;
+host_feature_virtualization-flatpak = import ./feature/virtualization-flatpak.nix;
 host_feature_virtualization-virtd = import ./feature/virtualization-virtd.nix;
 
 host_hardware_bluetooth = import ./hardware/bluetooth.nix;
---
+++ modules/nixos/feature/virtualization-flatpak.nix
@@ -0,0 +1,39 @@
+{config, lib, pkgs, ...}:
+
+let
+  cfg = config.host.feature.virtualization.flatpak;
+in
+  with lib;
+{
+  options = {
+    host.feature.virtualization.flatpak = {
+      enable = mkOption {
+        default = false;
+        type = with types; bool;
+        description = "Enable support for Flatpak containerization";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    fileSystems = let
+      mkRoSymBind = path: {
+        device = path;
+        fsType = "fuse.bindfs";
+        options = [ "ro" "resolve-symlinks" "x-gvfs-hide" ];
+      };
+      aggregatedFonts = pkgs.buildEnv {
+        name = "system-fonts";
+        paths = config.fonts.fonts;
+        pathsToLink = [ "/share/fonts" ];
+      };
+    in {
+      # Create an FHS mount to support flatpak host icons/fonts
+      "/usr/share/icons" = mkRoSymBind (config.system.path + "/share/icons");
+      "/usr/share/fonts" = mkRoSymBind (aggregatedFonts + "/share/fonts");
+    };
+
+    services.flatpak.enable = true;
+    system.fsPackages = [ pkgs.bindfs ];
+  };
+}
