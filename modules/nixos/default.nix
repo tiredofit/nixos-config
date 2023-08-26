3df744dd91c8f939534cc1997427e23c772775fb
Dave Conroy <dave@tiredofit.ca>
2023-08-26 10:22:27 -0700

    modules - add host.feature.authentication.sssd

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,4 +1,5 @@
 {
+host-feature-authentication-sssd = import ./feature/authentication-sssd.nix;
 host-feature-boot-efi = import ./feature/boot-efi.nix;
 host-feature-boot-graphical = import ./feature/boot-graphical.nix;
 host-feature-cross_compilation = import ./feature/cross_compilation.nix;
---
+++ modules/nixos/feature/authentication-sssd.nix
@@ -0,0 +1,28 @@
+{config, lib, pkgs, ...}:
+
+let
+  cfg = config.host.feature.authentication.sssd;
+in
+  with lib;
+{
+  options = {
+    host.feature.authentication.sssd = {
+      enable = mkOption {
+        default = false;
+        type = with types; bool;
+        description = "Enables ability to authenticate against LDAP servers";
+      };
+    };
+  };
+  ## TODO Add additional options
+  config = mkIf cfg.enable {
+    sssd = {
+      enable = true;
+      sshAuthorizedKeysintegration = true;
+        PermitRootLogin = "no" ;
+      };
+      config = ''
+      '';
+    };
+  };
+}
