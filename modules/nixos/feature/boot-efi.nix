843f3a58c5d868fa75dbb1634080fa890a5afc99
Dave Conroy <dave@tiredofit.ca>
2023-08-27 10:34:07 -0700

    module/feature/boot-efi - Add some options for grub and variety

--- modules/nixos/feature/boot-efi.nix
+++ modules/nixos/feature/boot-efi.nix
@@ -1,32 +1,40 @@
 {config, lib, pkgs, ...}:
 
 let
-  cfg = config.host.feature.boot.efi;
+  cfg = config.host.feature.boot;
 in
   with lib;
 {
   options = {
-    host.feature.boot.efi = {
-      enable = mkOption {
-        default = true;
-        type = with types; bool;
-        description = "Enables booting via EFI";
+    host.feature.boot = {
+      efi = {
+        enable = mkOption {
+          default = true;
+          type = with types; bool;
+          description = "Enables booting via EFI";
+        };
+      };
+      loader = mkOption {
+        default = "grub";
+        type = types.enum [ "grub" ];
+        description = "Enables booting via Grub";
+        ## TODO Consider creating top level boot.nix feature and integrating systemd-boot
       };
     };
   };
 
-  config = mkIf cfg.enable {
+  config = mkIf cfg.efi.enable {
     boot = {
       loader = {
         efi = {
           canTouchEfiVariables = false;
         };
-        grub = {
-          enable = true;
+        grub = mkIf (cfg.loader == "grub") {
+          enable = mkDefault true;
           device = "nodev";
-          efiSupport = true;
-          enableCryptodisk = false;
-          useOSProber = false;
+          efiSupport = cfg.efi.enable;
+          enableCryptodisk = mkDefault false;
+          useOSProber = mkDefault false;
           efiInstallAsRemovable = true;
         };
       };
