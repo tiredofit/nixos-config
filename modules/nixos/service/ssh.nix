b4ffa1708363059e7995d3623cd1201444e3e1fa
Dave Conroy <dave@tiredofit.ca>
2023-08-26 18:02:01 -0700

    module-service-ssh - Add harden options

--- modules/nixos/service/ssh.nix
+++ modules/nixos/service/ssh.nix
@@ -45,12 +45,33 @@ in
           PermitRootLogin = mkDefault "no" ;
           StreamLocalBindUnlink = "yes";
           GatewayPorts = "clientspecified";
+          Ciphers = mkIf cfg.harden [
+           "chacha20-poly1305@openssh.com"
+           "aes256-gcm@openssh.com"
+           "aes128-gcm@openssh.com"
+           "aes256-ctr"
+           "aes192-ctr"
+           "aes128-ctr"
+          ];
           KexAlgorithms = mkIf cfg.harden [
+            "curve25519-sha256@libssh.org"
+            "ecdh-sha2-nistp521"
+            "ecdh-sha2-nistp384"
+            "ecdh-sha2-nistp256"
+            "diffie-hellman-group-exchange-sha256"
             "curve25519-sha256"
             "curve25519-sha256@libssh.org"
             "diffie-hellman-group16-sha512"
             "diffie-hellman-group18-sha512"
             "sntrup761x25519-sha512@openssh.com"
+          ];
+          Macs = mkIf cfg.harden [
+            "hmac-sha2-512-etm@openssh.com"
+            "hmac-sha2-256-etm@openssh.com"
+            "umac-128-etm@openssh.com"
+            "hmac-sha2-512"
+            "hmac-sha2-256"
+            "umac-128@openssh.com"
           ];
         };
       };
