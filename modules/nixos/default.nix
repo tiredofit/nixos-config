8fb602d2695040d8886127f69bec2ccb1fbb5c98
Dave Conroy <dave@tiredofit.ca>
2023-08-30 15:23:52 -0700

    modules - add host.service.zabbix_agent

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -41,4 +41,5 @@ host-service-docker_container_manager = import ./service/docker_container_manage
 host-service-eternal_terminal = import ./service/eternal_terminal.nix;
 host-service-ssh = import ./service/ssh.nix;
 host-service-vscode_server = import ./service/vscode_server.nix;
+host-service-zabbix_agent = import ./service/zabbix_agent.nix;
 }
--- modules/nixos/roles/default.nix
+++ modules/nixos/roles/default.nix
@@ -7,6 +7,7 @@ with lib;
     ./kiosk
     ./laptop
     ./minimal
+    ./server
     ./vm
   ];
 
