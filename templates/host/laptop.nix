6fe5665c2727b985284e4c928d6690e8015e0c5b
Dave Conroy <dave@tiredofit.ca>
2023-10-18 18:34:46 -0700

    templates/hosts - networking

--- templates/host/laptop.nix
+++ templates/host/laptop.nix
@@ -31,11 +31,11 @@
     };
     network = {
       hostname = "laptop-template";
-      wired.enable = false;       # This line can be removed if not using wired networking
-      type = "dyanmic";
-      ip = "192.168.123.32/24";   # This line can be removed if not using wired networking and is set to static
-      gateway = "192.168.123.1";  # This line can be removed if not using wired networking and is set to static
-      mac = "00:01:02:03:04:05";  # This line can be removed if not using wired networking and is set to static
+      wired.enable = false;             # This line can be removed if not using wired networking
+      wired.type = "dynamic";
+      wired.ip = "192.168.123.32/24";   # This line can be removed if not using wired networking and is set to static
+      wired.gateway = "192.168.123.1";  # This line can be removed if not using wired networking and is set to static
+      wired.mac = "00:01:02:03:04:05";  # This line can be removed if not using wired networking and is set to static
     };
     role = "laptop";
     user = {
