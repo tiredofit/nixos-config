9703168ab5db4e778e5b522683e909569ac7f353
Dave Conroy <dave@tiredofit.ca>
2023-10-23 13:43:30 -0700

    modules/application/kitty - repair details

--- modules/application/kitty.nix
+++ modules/application/kitty.nix
@@ -1,27 +1,23 @@
 {config, lib, pkgs, ...}:
 
 let
-  cfg = config.host.application.changeme;
+  cfg = config.host.application.kitty;
 in
   with lib;
 {
   options = {
-    host.application.changeme = {
+    host.application.kitty = {
       enable = mkOption {
         default = false;
         type = with types; bool;
-        description = "Enables changeme";
+        description = "Enables kitty";
       };
     };
   };
 
   config = mkIf cfg.enable {
     environment.systemPackages = [
+      kitty
     ];
-
-    programs.changeme = {
-
-    };
-
   };
 }
