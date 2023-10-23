2525911aea859ea3141710a5050a2fbb2480a06c
Dave Conroy <dave@tiredofit.ca>
2023-10-23 09:24:50 -0700

    modules/applications/rsync - add host.applications.rsync

---
+++ modules/applications/rsync.nix
@@ -0,0 +1,25 @@
+{config, lib, pkgs, ...}:
+
+let
+  cfg = config.host.application.rsync;
+in
+  with lib;
+{
+  options = {
+    host.application.rsync = {
+      enable = mkOption {
+        default = false;
+        type = with types; bool;
+        description = "Enables remote syncing tool";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = [
+      rsync
+    ];
+
+    ## TODO Add bash aliases
+  };
+}
