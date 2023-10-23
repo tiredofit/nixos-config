4a3d574095d3aa9a5cdbfd3d7305a7fba76972f5
Dave Conroy <dave@tiredofit.ca>
2023-10-23 09:23:54 -0700

    modules/applications/iftop - add host.applications.iftop

---
+++ modules/applications/iftop.nix
@@ -0,0 +1,23 @@
+{config, lib, pkgs, ...}:
+
+let
+  cfg = config.host.application.iftop;
+in
+  with lib;
+{
+  options = {
+    host.application.iftop = {
+      enable = mkOption {
+        default = false;
+        type = with types; bool;
+        description = "Enables network interface measurement";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = [
+      iftop
+    ];
+  };
+}
