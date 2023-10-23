345eb9568451186c34abc27a871c55a567b84efb
Dave Conroy <dave@tiredofit.ca>
2023-10-23 09:24:19 -0700

    modules/applications/mtr - add host.applications.mtr

---
+++ modules/applications/mtr.nix
@@ -0,0 +1,23 @@
+{config, lib, pkgs, ...}:
+
+let
+  cfg = config.host.application.mtr;
+in
+  with lib;
+{
+  options = {
+    host.application.mtr = {
+      enable = mkOption {
+        default = false;
+        type = with types; bool;
+        description = "Enables visual network tracerouting";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = [
+      mtr
+    ];
+  };
+}
