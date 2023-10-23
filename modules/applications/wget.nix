10107c533097073a31fc2731a560a23bbcc5ffbd
Dave Conroy <dave@tiredofit.ca>
2023-10-23 09:24:58 -0700

    modules/applications/wget - add host.applications.wget

---
+++ modules/applications/wget.nix
@@ -0,0 +1,23 @@
+{config, lib, pkgs, ...}:
+
+let
+  cfg = config.host.application.;
+in
+  with lib;
+{
+  options = {
+    host.application.wget = {
+      enable = mkOption {
+        default = false;
+        type = with types; bool;
+        description = "Enables file downloader";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = [
+      wget
+    ];
+  };
+}
