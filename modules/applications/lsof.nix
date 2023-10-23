0c68ddb2aa2fb3f93043e5caf4ec3aa3b3e1c321
Dave Conroy <dave@tiredofit.ca>
2023-10-23 09:24:16 -0700

    modules/applications/lsof - add host.applications.lsof

---
+++ modules/applications/lsof.nix
@@ -0,0 +1,23 @@
+{config, lib, pkgs, ...}:
+
+let
+  cfg = config.host.application.lsof;
+in
+  with lib;
+{
+  options = {
+    host.application.lsof = {
+      enable = mkOption {
+        default = false;
+        type = with types; bool;
+        description = "Enables listing of open files";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = [
+      lsof
+    ];
+  };
+}
