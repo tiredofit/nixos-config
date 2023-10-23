dc33964587861208e0653761984d07fe8f0c2521
Dave Conroy <dave@tiredofit.ca>
2023-10-23 09:12:07 -0700

    modules/applications/git - add host.applications.git

---
+++ modules/applications/git.nix
@@ -0,0 +1,24 @@
+{config, lib, pkgs, ...}:
+
+let
+  cfg = config.host.application.git;
+in
+  with lib;
+{
+  options = {
+    host.application.git = {
+      enable = mkOption {
+        default = false;
+        type = with types; bool;
+        description = "Enables git";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = [
+      git
+      git-lfs
+    ];
+  };
+}
