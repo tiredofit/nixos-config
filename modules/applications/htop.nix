61beb32ffc9039ef0870a07afee7e98bd8a70694
Dave Conroy <dave@tiredofit.ca>
2023-10-23 09:12:26 -0700

    modules/applications/htop - add host.applications.htop

---
+++ modules/applications/htop.nix
@@ -0,0 +1,23 @@
+{config, lib, pkgs, ...}:
+
+let
+  cfg = config.host.application.htop;
+in
+  with lib;
+{
+  options = {
+    host.application.htop = {
+      enable = mkOption {
+        default = false;
+        type = with types; bool;
+        description = "Enables htop";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = [
+      htop
+    ];
+  };
+}
