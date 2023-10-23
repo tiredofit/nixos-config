726be254bab59a1060b4c08161115fb3e589f4e6
Dave Conroy <dave@tiredofit.ca>
2023-10-23 09:24:56 -0700

    modules/applications/tmux - add host.applications.tmux

---
+++ modules/applications/tmux.nix
@@ -0,0 +1,23 @@
+{config, lib, pkgs, ...}:
+
+let
+  cfg = config.host.application.tmux;
+in
+  with lib;
+{
+  options = {
+    host.application.tmux = {
+      enable = mkOption {
+        default = false;
+        type = with types; bool;
+        description = "Enables terminal multiplexer";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = [
+      tmux
+    ];
+  };
+}
