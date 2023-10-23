dd6e17d5797fb48ffe898e33232bd636c9857e18
Dave Conroy <dave@tiredofit.ca>
2023-10-23 09:24:45 -0700

    modules/applications/psmisc - add host.applications.psmisc

---
+++ modules/applications/psmisc.nix
@@ -0,0 +1,23 @@
+{config, lib, pkgs, ...}:
+
+let
+  cfg = config.host.application.psmisc;
+in
+  with lib;
+{
+  options = {
+    host.application.psmisc = {
+      enable = mkOption {
+        default = false;
+        type = with types; bool;
+        description = "Enables process managment tools";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = [
+      psmisc
+    ];
+  };
+}
