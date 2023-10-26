fc7b29d8cc0af0b67d76be0e62daabcbc36dfc65
Dave Conroy <dave@tiredofit.ca>
2023-10-26 15:48:39 -0700

    modules/container - add host.container.clamav

--- modules/container/default.nix
+++ modules/container/default.nix
@@ -3,7 +3,14 @@
 with lib;
 {
   imports = [
+    ./clamav.nix
+    #./fluentbit.nix
+    #./llng-handler.nix
+    #./postfix-relay.nix
+    #./restic.nix
     ./socket-proxy.nix
+    #./tinc.nix
     ./unbound.nix
+    #./zabbix.nix
   ];
 }
