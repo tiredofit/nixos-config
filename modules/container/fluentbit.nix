cccba3348f5ee7908d68ec938e739edeabbf29ca
Dave Conroy <dave@tiredofit.ca>
2023-10-27 07:41:32 -0700

    modules/feature/virtualization/docker - remove braces around variable

---
+++ modules/container/fluentbit.nix
@@ -0,0 +1,113 @@
+{config, lib, pkgs, ...}:
+
+let
+  container_name = "fluentbit";
+  container_description = "Enables fluentbit log forwarding container";
+  container_image_registry = "docker.io";
+  container_image_name = "tiredofit/alpine";
+  container_image_tag = "3.18";
+  cfg = config.host.container.${container_name};
+  hostname = config.host.network.hostname;
+  activationScript = "system.activationScripts.docker_${container_name}";
+in
+  with lib;
+{
+  options = {
+    host.container.${container_name} = {
+      enable = mkOption {
+        default = true;
+        type = with types; bool;
+        description = container_description;
+      };
+      image = {
+        name = mkOption {
+          default = container_image_name;
+          type = with types; str;
+          description = "Image name";
+        };
+        tag = mkOption {
+          default = container_image_tag;
+          type = with types; str;
+          description = "Image tag";
+        };
+        registry = {
+          host = mkOption {
+            default = container_image_registry;
+            type = with types; str;
+            description = "Image Registry";
+          };
+        };
+      };
+      logship = mkOption {
+        default = "false";
+        type = with types; str;
+        description = "Enable monitoring for this container";
+      };
+      monitor = mkOption {
+        default = "false";
+        type = with types; str;
+        description = "Enable monitoring for this container";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    sops.secrets = {
+      "common-container-${container_name}"; = {
+        format = "env";
+        sopsFile = ../../hosts/common/secrets/"container-${container_name}.env";
+      };
+    };
+    system.activationScripts."docker_${container_name}" = ''
+      if [ ! -d /var/local/data/_system/${container_name}/logs ]; then
+          mkdir -p /var/local/data/_system/${container_name}/logs
+          ${pkgs.e2fsprogs}/bin/chattr +C /var/local/data/_system/${container_name}/logs
+      fi
+    '';
+
+    systemd.services."docker-${container_name}" = {
+      serviceConfig = {
+        StandardOutput = "null";
+        StandardError = "null";
+      };
+    };
+
+    virtualisation.oci-containers.containers."${container_name}" = {
+      image = "${cfg.image.name}:${cfg.image.tag}";
+      volumes = [
+        "/var/local/data/_system/${container_name}/logs:/var/log/fluentbit"
+      ];
+      environment = {
+      "TIMEZONE" = "America/Vancouver";
+      "CONTAINER_NAME" = "${hostname}-${container_name}";
+      "CONTAINER_ENABLE_MONITORING" = cfg.monitor;
+      "CONTAINER_ENABLE_LOGSHIPPING" = cfg.logship;
+
+      "FLUENTBIT_OUTPUT" = "LOKI";
+      #"FLUENTBIT_OUTPUT_LOKI_HOST" = "common_env";
+      #"FLUENTBIT_OUTPUT_LOKI_PORT" = "common_env";
+      "FLUENTBIT_OUTPUT_LOKI_TLS" = "TRUE";
+      "FLUENTBIT_OUTPUT_LOKI_TLS_VERIFY" = "TRUE";
+      #"FLUENTBIT_OUTPUT_LOKI_USER" = "host_env";
+      #"FLUENTBIT_OUTPUT_LOKI_PASS" = "host_env";
+      };
+      environmentFiles = [
+        config.sops.secrets."common-container-${container_name}".path;
+        #config.sops.secrets."host-container-${container_name}".path;
+      ];
+      extraOptions = [
+        "--memory=1024M"
+        "--network=services"
+        "--network-alias=${hostname}-${container_name}"
+        "--network-alias=fluent-proxy"
+        "--network-alias=logshipper"
+      ];
+
+      autoStart = mkDefault true;
+      log-driver = mkDefault "local";
+      login = {
+        registry = cfg.image.registry.host;
+      };
+    };
+  };
+}
