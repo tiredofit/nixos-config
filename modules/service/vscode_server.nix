3e982f4c7cf69b3a0236bacd0dd760311ca8ec68
Dave Conroy <dave@tiredofit.ca>
2023-11-03 20:02:10 -0700

    modules/service/vscode - temp remove impermanance for /var/empty

--- modules/service/vscode_server.nix
+++ modules/service/vscode_server.nix
@@ -24,8 +24,8 @@ in
       vscode-server.enable = true;
     };
 
-    host.filesystem.impermanence.directories = lib.mkIf config.host.filesystem.impermanence.enable [
-      "/var/empty"               # VS Code server throws error on startup without this
-    ];
+    #host.filesystem.impermanence.directories = lib.mkIf config.host.filesystem.impermanence.enable [
+    #  "/var/empty"               # VS Code server throws error on startup without this
+    #];
   };
 }
