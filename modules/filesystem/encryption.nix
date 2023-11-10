f57701ef4ff6af3f6308e2fafe66b06aca1d0f4c
Dave Conroy <dave@tiredofit.ca>
2023-11-10 08:14:35 -0800

    modules/filesystem/encryption - fix option definition for networkModule

--- modules/filesystem/encryption.nix
+++ modules/filesystem/encryption.nix
@@ -63,10 +63,10 @@ in
             :::
           '';
         };
-        networkModule = {
+        networkModule = mkOption {
           type = types.listOf types.str;
           default = [];
-          example = [ "ixgbe" "r8169" ];
+          example = [ "ixgbe" "r8169" "virtio-pci" ];
           description = "Kernel module to embed in initrd to allow for network access. Use 'lspci -v' to get the right module";
         };
         port = mkOption {
