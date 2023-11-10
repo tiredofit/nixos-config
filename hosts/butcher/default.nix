1c35026b6f183aec66215bb0bc3dc16f5a22ac21
Dave Conroy <dave@tiredofit.ca>
2023-11-10 13:18:41 -0800

    hosts/butcher - convert network to network.wired

--- hosts/butcher/default.nix
+++ hosts/butcher/default.nix
@@ -29,9 +29,12 @@
     };
     network = {
       hostname = "butcher";
-      ip = "192.168.137.5/24";
-      gateway = "192.168.137.1";
-      mac = "2A:BE:78:89:51:A5";
+      wired = {
+        enable = true;
+        ip = "192.168.137.5/24";
+        gateway = "192.168.137.1";
+        mac = "2A:BE:78:89:51:A5";
+      };
     };
     user = {
       dave.enable = true;
