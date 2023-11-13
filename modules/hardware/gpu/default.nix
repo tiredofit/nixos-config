77b1a354e5b177fe5f34025279c773090a56ca3d
Dave Conroy <dave@tiredofit.ca>
2023-11-13 09:34:45 -0800

    modules/hardware/gpu - switch option from hybrid-nv to hybrid-nvidia

--- modules/hardware/gpu/default.nix
+++ modules/hardware/gpu/default.nix
@@ -9,7 +9,7 @@
 
   options = {
     host.hardware.gpu = mkOption {
-        type = types.enum [ "amd" "intel" "nvidia" "hybrid-nv" "hybrid-amd" "integrated-amd" "pi" null];
+        type = types.enum [ "amd" "intel" "nvidia" "hybrid-nvidia" "hybrid-amd" "integrated-amd" "pi" null];
         default = null;
         description = "Manufacturer/type of the primary system GPU";
     };
