3436bbbdaef53d8cb9905d74dccb03df2a5a232c
Dave Conroy <dave@tiredofit.ca>
2024-01-05 15:35:04 -0800

    modules/filesystem/tmp - add modules.filessystem.tmp.tmpfs

--- modules/filesystem/tmp.nix
+++ modules/filesystem/tmp.nix
@@ -1,9 +1,10 @@
 {config, lib, pkgs, ...}:
 
 let
-  cfg = config.host.filesystem.tmp,tmpfs;
+  cfg = config.host.filesystem.tmp.tmpfs;
 in
   with lib;
+  with types;
 {
   options = {
     host.filesystem.tmp.tmpfs = {
@@ -12,58 +13,19 @@ in
         type = with types; bool;
         description = "Enables tmpfs on /tmp mount during boot";
       };
-      autoscrub = mkOption {
-       default = true;
-        type = with types; bool;
-        description = "Enable autoscrubbing of file systems";
+      size = mkOption {
+       default = "50%";
+        type = with types; oneOf [ types.str types.types.ints.positive ];
+        description = "Size of TMPFS";
       };
     };
   };
 
   config = mkIf cfg.enable {
     boot = {
-      supportedFilesystems = [
-        "btrfs"
-      ];
-    };
-
-    fileSystems = {
-      "/".options = [ "subvol=root" "compress=zstd" "noatime"  ];
-      "/home".options = [ "subvol=home/active" "compress=zstd" "noatime"  ];
-      "/home/.snapshots".options = [ "subvol=home/snapshots" "compress=zstd" "noatime"  ];
-      "/nix".options = [ "subvol=nix" "compress=zstd" "noatime"  ];
-      "/var/local".options = [ "subvol=var_local/active" "compress=zstd" "noatime"  ];
-      "/var/local/.snapshots".options = [ "subvol=var_local/snapshots" "compress=zstd" "noatime"  ];
-      "/var/log".options = [ "subvol=var_log" "compress=zstd" "noatime"  ];
-      "/var/log".neededForBoot = true;
-    };
-
-    services = {
-      btrbk = {
-        instances."btrbak" = {
-          onCalendar = "*-*-* *:00:00";
-          settings = {
-            timestamp_format = "long";
-            preserve_day_of_week = "sunday" ;
-            preserve_hour_of_day = "0" ;
-            snapshot_preserve = "48h 10d 4w 12m 10y" ;
-            snapshot_preserve_min = "2d";
-            volume."/home" = {
-              snapshot_create = "always";
-              subvolume = ".";
-              snapshot_dir = ".snapshots";
-            };
-            volume."/var/local" = {
-              snapshot_create = "always";
-              subvolume = ".";
-              snapshot_dir = ".snapshots";
-            };
-          };
-        };
-      };
-      btrfs.autoScrub = mkIf cfg.autoscrub {
-        enable = true;
-        fileSystems = ["/"];
+      tmp = {
+        useTmpfs = true;
+        tmpfsSize = cfg.size;
       };
     };
   };
