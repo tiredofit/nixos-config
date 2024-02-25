cf8973b18ed10cc0ef22b303c1a458685dbd9651
Dave Conroy <dave@tiredofit.ca>
2024-02-25 09:56:31 -0800

    flake - remove devshells

--- flake.nix
+++ flake.nix
@@ -60,7 +60,6 @@
       nixosModules = import ./modules;
       overlays = import ./overlays { inherit inputs outputs; };
       packages = forEachSystem (pkgs: import ./pkgs { inherit pkgs; });
-      devShells = forEachSystem (pkgs: import ./shell.nix { inherit pkgs; });
       formatter = forEachSystem (pkgs: pkgs.nixpkgs-fmt);
 
       nixosConfigurations = {
