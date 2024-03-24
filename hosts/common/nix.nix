1d073536e3df55de7d6ef35949b5a428e071512a
Dave Conroy <dave@tiredofit.ca>
2024-03-23 20:58:27 -0700

    hosts/common/nix - don't write a nvd diff file for packages if nothings changed

--- hosts/common/nix.nix
+++ hosts/common/nix.nix
@@ -60,7 +60,11 @@
       PATH=$PATH:${lib.makeBinPath [ pkgs.nvd pkgs.nix ]}
       nvd diff $(ls -dv /nix/var/nix/profiles/system-*-link | tail -2)
       mkdir -p /var/log/activations
-      nvd diff $(ls -dv /nix/var/nix/profiles/system-*-link | tail -2) > /var/log/activations/$(date +'%Y%m%d%H%M%S')-$(ls -dv /nix/var/nix/profiles/system-*-link | tail -1 | cut -d '-' -f 2)-$(readlink $(ls -dv /nix/var/nix/profiles/system-*-link | tail -1) | cut -d - -f 4-).log
+      _nvddate=$(date +'%Y%m%d%H%M%S')
+      nvd diff $(ls -dv /nix/var/nix/profiles/system-*-link | tail -2) > /var/log/activations/$_nvddate-$(ls -dv /nix/var/nix/profiles/system-*-link | tail -1 | cut -d '-' -f 2)-$(readlink $(ls -dv /nix/var/nix/profiles/system-*-link | tail -1) | cut -d - -f 4-).log
+      if grep -q "No version or selection state changes" "/var/log/activations/$_nvddate-$(ls -dv /nix/var/nix/profiles/system-*-link | tail -1 | cut -d '-' -f 2)-$(readlink $(ls -dv /nix/var/nix/profiles/system-*-link | tail -1) | cut -d - -f 4-).log" ; then
+        rm -rf "/var/log/activations/$_nvddate-$(ls -dv /nix/var/nix/profiles/system-*-link | tail -1 | cut -d '-' -f 2)-$(readlink $(ls -dv /nix/var/nix/profiles/system-*-link | tail -1) | cut -d - -f 4-).log"
+      fi
     '';
     autoUpgrade.enable = mkDefault false;
     stateVersion = mkDefault "23.11";
