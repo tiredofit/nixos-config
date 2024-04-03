bf623e238c02fef60ce10703d22530f34f233c84
Dave Conroy <dave@tiredofit.ca>
2024-04-03 08:43:03 -0700

    flake - downgrade hyprland input to 0.38.0 to address stability issues

--- flake.nix
+++ flake.nix
@@ -26,17 +26,24 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    hyprland.url = "github:hyprwm/Hyprland";
-    hyprwm-contrib = {
+    hyprland = {
+      url = "github:hyprwm/Hyprland/v0.38.0";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprland-contrib = {
       url = "github:hyprwm/contrib";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    impermanence.url = "github:nix-community/impermanence";
+    impermanence = {
+      url = "github:nix-community/impermanence";
+    };
     nix-gaming = {
       url = "github:fufexan/nix-gaming";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nur.url = "github:nix-community/NUR";
+    nur = {
+      url = "github:nix-community/NUR";
+    };
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
