1934ef7a80ac19c05d429a5e202c42e2c3648324
Dave Conroy <dave@tiredofit.ca>
2024-04-08 08:38:25 -0700

    flake - hyprland input - switch back to bleeding edge

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hyprland = {
-      url = "github:hyprwm/Hyprland/v0.38.0";
+      url = "github:hyprwm/Hyprland";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hyprland-contrib = {
