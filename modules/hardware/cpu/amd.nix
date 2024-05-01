ba728ccb133e22c0429c4d75518e83117e9aec48
Dave Conroy <dave@tiredofit.ca>
2024-05-01 13:03:36 -0700

    modules/hardware/cpu/amd - remove statements revovling around anything prior to linux 6.3

--- modules/hardware/cpu/amd.nix
+++ modules/hardware/cpu/amd.nix
@@ -7,23 +7,6 @@ in {
   config = mkIf (device.cpu == "amd" || device.cpu == "vm-amd") {
     hardware.cpu.amd.updateMicrocode = true;
     boot = lib.mkMerge [
-      (lib.mkIf
-        (
-          (lib.versionAtLeast kver "5.17")
-          && (lib.versionOlder kver "6.1")
-        )
-        {
-          kernelParams = [ "initcall_blacklist=acpi_cpufreq_init" ];
-          kernelModules = [ "amd-pstate" "kvm-amd" ];
-        })
-      (lib.mkIf
-        (
-          (lib.versionAtLeast kver "6.1")
-          && (lib.versionOlder kver "6.3")
-        )
-        {
-          kernelParams = [ "amd_pstate=passive" "kvm-amd" ];
-        })
       (lib.mkIf (lib.versionAtLeast kver "6.3") {
         kernelParams = [ "amd_pstate=active" "kvm-amd" ];
       })
