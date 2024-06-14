a0b366d4ab8b380ea1a21893b0be829c88e71b09
Dave Conroy <dave@tiredofit.ca>
2024-06-14 14:00:23 -0700

    flake - update inputs

--- flake.nix
+++ flake.nix
@@ -4,11 +4,13 @@
   nixConfig = {
     experimental-features = [ "nix-command" "flakes" ];
     extra-substituters = [
+      "https://hyprland.cachix.org"
       "https://nix-community.cachix.org"
       "https://nix-gaming.cachix.org"
     ];
     extra-trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
     ];
@@ -23,6 +25,9 @@
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprland = {
+      url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
     };
     impermanence = {
       url = "github:nix-community/impermanence";
--- modules/feature/graphics/windowManager/hyprland.nix
+++ modules/feature/graphics/windowManager/hyprland.nix
@@ -9,25 +9,8 @@ in
     programs = {
       hyprland = {
         enable = mkDefault true;
-        #package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+        package = inputs.hyprland.packages.${pkgs.system}.hyprland;
       };
     };
-
-    #xdg.portal = {
-    #  enable = true;
-    #  extraPortals = with pkgs;  [
-    #    xdg-desktop-portal
-    #    xdg-desktop-portal-gtk
-    #    xdg-desktop-portal-wlr
-    #  ];
-    #  wlr = {
-    #    enable = true;
-    #    settings = {
-    #      screencast = {
-#
-    #      };
-    #    };
-    #  };
-    #};
   };
 }
