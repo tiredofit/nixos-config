6eb0a39a49037698ca9da73b10647563d4bd2dbd
Dave Conroy <dave@tiredofit.ca>
2024-08-01 18:34:12 -0700

    modules/feature/graphics/windowManager/hyprland - add some xdg portal info

--- modules/feature/graphics/windowManager/hyprland.nix
+++ modules/feature/graphics/windowManager/hyprland.nix
@@ -12,5 +12,20 @@ in
         #package = inputs.hyprland.packages.${pkgs.system}.hyprland;
       };
     };
+
+    xdg.portal = {
+      enable = true;
+      xdgOpenUsePortal = true;
+      config.common = {
+        "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
+        "org.freedesktop.impl.portal.ScreenCast" = [ "hyprland" ];
+        "org.freedesktop.impl.portal.Screenshot" = [ "hyprland" ];
+        "org.freedesktop.portal.FileChooser" = [ "xdg-desktop-portal-gtk" ];
+      };
+      extraPortals = [
+        pkgs.xdg-desktop-portal-hyprland
+        pkgs.xdg-desktop-portal-gtk
+      ];
+    };
   };
 }
