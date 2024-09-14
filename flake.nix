2840cfd28b7c5018686df1aef1fe8d2061318e1d
Dave Conroy <dave@tiredofit.ca>
2024-09-14 09:33:32 -0700

    Flake - Change nixpkgs to unstable

--- flake.nix
+++ flake.nix
@@ -9,21 +9,22 @@
     extra-substituters = [
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
-      #"https://nix-gaming.cachix.org"
-      #"https://nixpkgs-wayland.cachix.org"
-      #"https://hyprland.cachix.org"
+      "https://nix-gaming.cachix.org"
+      "https://nixpkgs-wayland.cachix.org"
+      "https://hyprland.cachix.org"
 
     ];
     extra-trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      #"nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-      #"nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
     ];
   };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     disko = {
       url = "github:nix-community/disko";
@@ -56,7 +57,7 @@
         inherit system;
         config.allowUnfree = true;
             overlays = [
-              #inputs.nixpkgs-wayland.overlay
+              inputs.nixpkgs-wayland.overlay
               outputs.overlays.additions
               outputs.overlays.modifications
               outputs.overlays.unstable-packages
