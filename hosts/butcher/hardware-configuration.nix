887e64db30765e34f69209e915c29bacaacef98d
Dave Conroy <dave@tiredofit.ca>
2024-10-20 08:10:43 -0700

    hosts/butcher - lint hardware-configuration

--- hosts/butcher/hardware-configuration.nix
+++ hosts/butcher/hardware-configuration.nix
@@ -1,80 +1,86 @@
 { config, lib, pkgs, ... }:
 
 {
-  boot.initrd.availableKernelModules = [ "uhci_hcd" "ehci_pci" "ahci" "vmw_pvscsi" "sd_mod" "sr_mod" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
-  boot.extraModulePackages = [ ];
+  boot = {
+    initrd = {
+      availableKernelModules = [
+        "uhci_hcd"
+        "ehci_pci"
+        "ahci"
+        "vmw_pvscsi"
+        "sd_mod"
+        "sr_mod"
+      ];
+      luks = {
+        devices = {
+          "pool0_0" = {
+            device = "/dev/disk/by-uuid/d1dd4e01-d147-41af-91b4-e736bf96bf78";
+          };
+        };
+      };
+    };
+  };
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/740f60e3-d62c-48db-b74a-668912c0aef1";
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/740f60e3-d62c-48db-b74a-668912c0aef1";
       fsType = "btrfs";
       options = [ "subvol=root" ];
     };
-
-  fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/740f60e3-d62c-48db-b74a-668912c0aef1";
+    "/boot" = {
+      device = "/dev/disk/by-uuid/61FA-03B6";
+      fsType = "vfat";
+    };
+    "/home" = {
+      device = "/dev/disk/by-uuid/740f60e3-d62c-48db-b74a-668912c0aef1";
       fsType = "btrfs";
       options = [ "subvol=home/active" ];
     };
-
-  fileSystems."/home/.snapshots" =
-    { device = "/dev/disk/by-uuid/740f60e3-d62c-48db-b74a-668912c0aef1";
+    "/home/.snapshots" = {
+      device = "/dev/disk/by-uuid/740f60e3-d62c-48db-b74a-668912c0aef1";
       fsType = "btrfs";
       options = [ "subvol=home/snapshots" ];
     };
-
-  fileSystems."/nix" =
-    { device = "/dev/disk/by-uuid/740f60e3-d62c-48db-b74a-668912c0aef1";
+    "/nix" = {
+      device = "/dev/disk/by-uuid/740f60e3-d62c-48db-b74a-668912c0aef1";
       fsType = "btrfs";
       options = [ "subvol=nix" ];
     };
-
-  fileSystems."/persist" =
-    { device = "/dev/disk/by-uuid/740f60e3-d62c-48db-b74a-668912c0aef1";
+    "/persist" = {
+      device = "/dev/disk/by-uuid/740f60e3-d62c-48db-b74a-668912c0aef1";
       fsType = "btrfs";
       options = [ "subvol=persist/active" ];
     };
-
-  fileSystems."/persist/.snapshots" =
-    { device = "/dev/disk/by-uuid/740f60e3-d62c-48db-b74a-668912c0aef1";
+    "/persist/.snapshots" = {
+      device = "/dev/disk/by-uuid/740f60e3-d62c-48db-b74a-668912c0aef1";
       fsType = "btrfs";
       options = [ "subvol=persist/snapshots" ];
     };
-
-  fileSystems."/var/lib/docker" =
-    { device = "/dev/disk/by-uuid/740f60e3-d62c-48db-b74a-668912c0aef1";
+    "/var/lib/docker" = {
+      device = "/dev/disk/by-uuid/740f60e3-d62c-48db-b74a-668912c0aef1";
       fsType = "btrfs";
       options = [ "subvol=var_lib_docker" ];
     };
-
-  fileSystems."/var/local" =
-    { device = "/dev/disk/by-uuid/740f60e3-d62c-48db-b74a-668912c0aef1";
+    "/var/local" = {
+      device = "/dev/disk/by-uuid/740f60e3-d62c-48db-b74a-668912c0aef1";
       fsType = "btrfs";
       options = [ "subvol=var_local/active" ];
     };
-
-  fileSystems."/var/local/.snapshots" =
-    { device = "/dev/disk/by-uuid/740f60e3-d62c-48db-b74a-668912c0aef1";
+    "/var/local/.snapshots" = {
+      device = "/dev/disk/by-uuid/740f60e3-d62c-48db-b74a-668912c0aef1";
       fsType = "btrfs";
       options = [ "subvol=var_local/snapshots" ];
     };
-
-  fileSystems."/var/log" =
-    { device = "/dev/disk/by-uuid/740f60e3-d62c-48db-b74a-668912c0aef1";
+    "/var/log" = {
+      device = "/dev/disk/by-uuid/740f60e3-d62c-48db-b74a-668912c0aef1";
       fsType = "btrfs";
       options = [ "subvol=var_log" ];
     };
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/61FA-03B6";
-      fsType = "vfat";
-    };
-
-  fileSystems."/mnt/media" =
-    { device = "/dev/disk/by-uuid/9c3cfc7b-f660-44eb-9c60-d32342cdf174";
+    "/mnt/media" = {
+      device = "/dev/disk/by-uuid/9c3cfc7b-f660-44eb-9c60-d32342cdf174";
       fsType = "btrfs";
     };
+  };
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
