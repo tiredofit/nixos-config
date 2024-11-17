f8c909eb626e056c8199f3b4b749d7628d08bf5b
Dave Conroy <dave@tiredofit.ca>
2024-11-17 11:54:01 -0800

    modules/hardware/gpu/amd - optimize config

--- modules/hardware/gpu/amd.nix
+++ modules/hardware/gpu/amd.nix
@@ -2,6 +2,8 @@
 with lib;
 let
   device = config.host.hardware ;
+  backend = config.host.feature.graphics.backend;
+  graphics = config.host.feature.graphics.enable;
 in {
   config = mkIf (device.gpu == "amd" || device.gpu == "hybrid-amd" || device.gpu == "integrated-amd")  {
     boot = lib.mkMerge [
@@ -26,7 +28,19 @@ in {
       };
     };
 
-    services.xserver.videoDrivers = [
+    environment = {
+      sessionVariables = mkMerge [
+        (mkIf (graphics) {
+          LIBVA_DRIVER_NAME = "radeonsi";
+        })
+
+        (mkIf ((graphics) && (backend == "wayland")) {
+          WLR_NO_HARDWARE_CURSORS = "1";
+        })
+      ];
+    };
+
+    services.xserver.videoDrivers = (mkIf ((graphics) && (backend == "x"))) [
       "amdgpu"
     ];
   };
