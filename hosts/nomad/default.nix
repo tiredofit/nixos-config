afe8ce97cae235145a31389c587e3d9e1ca7dbc6
Dave Conroy <dave@tiredofit.ca>
2025-01-11 10:41:03 -0800

    hosts/nomad - temporarily put actkbd and light in config, to do move to role

--- hosts/nomad/default.nix
+++ hosts/nomad/default.nix
@@ -45,4 +45,13 @@
       root.enable = true;
     };
   };
+
+    programs.light.enable = true;
+  services.actkbd = {
+    enable = true;
+    bindings = [
+      { keys = [ 224 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -U 10"; }
+      { keys = [ 225 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -A 10"; }
+    ];
+  };
 }
