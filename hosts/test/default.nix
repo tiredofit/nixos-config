3a159794d65e793a4ec887612ac3170136cece06
Dave Conroy <dave@tiredofit.ca>
2025-11-03 18:11:08 -0800

    hosts/common - adjust secrets

--- hosts/test/default.nix
+++ hosts/test/default.nix
@@ -95,7 +95,7 @@
     };
     filesystem = {
       encryption.enable = false;                 # This line can be removed if not needed as it is already default set by the role template
-      impermanence.enable = true;               # This line can be removed if not needed as it is already default set by the role template
+      impermanence.enable = true;                # This line can be removed if not needed as it is already default set by the role template
       #swap = {
       #  partition = "disk/by-partlabel/swap";
       #};
@@ -113,84 +113,34 @@
       firewall.fail2ban.enable = false;
       hostname = "test";
       interfaces = {
-        enp0 = {
+        eth60 = {
           match = {
-            mac = "52:54:00:09:c4:07";
+            mac = "52:54:00:44:71:80";
           };
         };
-        veth0 = { # Connected to nucleus br-quad2
-          match = {
-            mac = "52:54:00:b9:02:e2";
+        eth1337 = {
+           match = {
+            mac = "52:54:00:8a:c9:b3";
           };
-        };
-        vveth = { # Connected to nucleus br-vlan60
-          match = {
-            mac = "52:54:00:c0:53:f4";
-          };
-        };
-        br-veth0 = { # Create VLAN sub-interfaces on br-veth0 (receives tagged traffic from Nucleus)
-          match = {
-            name = "br-veth0";
-          };
-          vlans = [
-            "vlan60"
-            "vlan230"
-            "vlan1337"
-          ];
-        };
-      };
-      vlans = {
-        vlan60 = {
-          id = 60;
-        };
-        vlan230 = {
-          id = 230;
-        };
-        vlan1337 = {
-          id = 1337;
         };
       };
       bridges = {
-        br-veth0 = { # Bridge veth0 to receive all tagged VLAN traffic from Nucleus br-quad2
-          interfaces = [ "veth0" ];
-        };
-        br-vlan60 = { # Then create VLAN-specific bridges for containers/VMs
-          interfaces = [ "vlan60" ];
-        };
-        br-vlan230 = {
-          interfaces = [ "vlan230" ];
+        br-vlan60 = {
+          interfaces = [ "eth60" ];
         };
         br-vlan1337 = {
-          interfaces = [ "vlan1337" ];
+          interfaces = [ "eth1337" ];
         };
       };
       networks = {
-        enp0 = {
-          type = "dynamic";
-        };
-        veth0-bridge = { # veth0 is bridged (br-veth0) - no IP on veth0 itself, br-veth0 is unmanaged - just passes VLAN traffic through
-          type = "unmanaged";
-          match = {
-            name = "br-veth0";
-          };
-        };
-        vveth = { # Direct access to VLAN 60 via vveth
-          type = "dynamic";
-        };
         vlan60 = {
-          type = "dynamic"; # Host gets IP on VLAN 60
+          type = "dynamic";
           match = {
             name = "br-vlan60";
           };
         };
-        vlan230 = {
-          type = "dynamic";  # TEST host gets IP on VLAN 230
-          match = {
-            name = "br-vlan230";
-          };
-        };
         vlan1337 = {
-          type = "dynamic";  # TEST host gets IP on VLAN 1337
+          type = "dynamic";
           match = {
             name = "br-vlan1337";
           };
