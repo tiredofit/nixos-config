648784059dd2de08c25afd9aeb5f2fd994e5deff
Dave Conroy <dave@tiredofit.ca>
2026-01-02 12:22:57 -0800

    pull nspawn patch

--- lib/modules/patch-withNspawn.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, lib, ... }:
-{
-  config = {
-    # Ensure the `systemd.package` used by NixOS modules exposes
-    # `withNspawn`. Use the `pkgs` argument provided to modules (which
-    # in our `mkSystem` call is set to the already-imported `systemPkgs`).
-    systemd.package = pkgs.systemd // { withNspawn = true; };
-  };
-}
